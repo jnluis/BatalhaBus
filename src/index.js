a88c31574dc471b9d0ddab84751263a587c83156
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-04-20 16:18:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Footer from "./components/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <footer />
+    <h1 className="bg-gradient"> HELLO FRIENDS </h1>
+    <div>Bacalhau à Braá/ IF</div>
+    <Footer />
   </React.StrictMode>
 );
