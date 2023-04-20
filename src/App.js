c65ad78132e4f5658605d8419dfb6054b22127d3
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-04-20 16:18:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import './index.css';
+import "./index.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="bg-blue-700 text-white rounded">
-        Clique aqui
-      </button>
-      </header>
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;
