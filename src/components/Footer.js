899730bb6c42dfa25cb3b15d4f0cc471b9a094c3
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-04-20 14:44:50 +0100

    cenas

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer" />
+            </div>
+        );
+    }
+}
+
+
+Footer.propTypes = {
+
+};
+
+
+export default Footer;
