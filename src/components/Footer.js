928b3be6b345d1ad70c29307aa4758108aa2d3fe
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-04-20 17:02:08 +0100

    Trying to split footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,26 @@ import App from "../App";
 class Footer extends Component {
   render() {
     return (
-      <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4">
-        <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
-          <button className="text-lg font-medium">Home</button>
-          <button className="text-lg font-medium">Timetable</button>
-          <div className="bg-gray-0"> </div>
-          <button className="text-lg font-medium">Flag</button>
-          <button className="text-lg font-medium">Settings</button>
-        </div>
-      </footer>
+      <React.Fragment>
+        <footer className="fixed bottom-0 left-0 w-half bg-gray-800 text-white py-4">
+          <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
+            <button className="text-lg font-medium rounded-full">Home</button>
+            <button className="text-lg font-medium rounded-full">
+              Timetable
+            </button>
+          </div>
+        </footer>
+        <footer className="fixed bottom-0 right-0 w-half bg-gray-800 text-white py-4">
+          <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
+            <button className="text-lg font-medium rounded-full bg-color-white-500">
+              Flag
+            </button>
+            <button className="text-lg font-medium rounded-full">
+              Settings
+            </button>
+          </div>
+        </footer>
+      </React.Fragment>
     );
   }
 }
