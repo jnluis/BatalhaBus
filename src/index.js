1592e762d16b108c5886bb14a049c13203f38230
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-04-26 13:11:38 +0100

    Small tweaks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Footer from "./components/Footer";
+import Media from "./pages/media/media";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <App />
     <h1 className="bg-gradient"> HELLO FRIENDS </h1>
     <div>Bacalhau à Braá/ IF</div>
+    <Media />
     <Footer />
   </React.StrictMode>
 );
