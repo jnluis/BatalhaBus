71654d4772ca134b723781927826b223470db125
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-04-26 13:11:51 +0100

    Trying to fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,24 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import "../index.css";
-
-import ReactDOM from "react-dom/client";
-import App from "../App";
 
 class Footer extends Component {
   render() {
-    let footerClass = "fixed bottom-0 w-half bg-gray-800 text-white py-4";
+    let footerClass = "w-full bg-gray-800 text-white py-4";
     let partialClass =
       "max-w-6xl mx-auto px-4 flex justify-between items-center";
-    let buttonClass = "text-lg font-medium rounded-full";
+    let buttonClass = "text-lg font-medium rounded-full bg-white";
 
     return (
-      <React.Fragment>
-        <footer className={(footerClass += " left-0")}>
-          <div className={partialClass}>
-            <button className={buttonClass}>Home</button>
-            <button className={buttonClass}>Timetable</button>
-          </div>
-        </footer>
-        <footer className={(footerClass += " right-0")}>
-          <div className={partialClass}>
-            <button className={buttonClass}>Flag</button>
-            <button className={buttonClass}>Settings</button>
-          </div>
-        </footer>
-      </React.Fragment>
+      <footer className={footerClass} style={{ position: "fixed", bottom: 0 }}>
+        <div className={"static left-0 " + partialClass}>
+          <button className={buttonClass}>Home</button>
+          <button className={buttonClass}>Timetable</button>
+        </div>
+        <div className={"static right-0 " + partialClass}>
+          <button className={buttonClass}>Flag</button>
+          <button className={buttonClass}>Settings</button>
+        </div>
+      </footer>
     );
   }
 }
