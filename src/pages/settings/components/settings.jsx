b0f7e715c7c40d62b21a8c376d825dafdd11727e
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-03 11:39:41 +0100

    Create settings.jsx
    Right Side of the Settings Page

---
+++ src/pages/settings/components/settings.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+function Navbar() {
+  return (
+    <div className="flex flex-col">
+      <div className="Header">Title</div>
+      <div>Settings1</div>
+      <div>Settings2</div>
+    </div>
+  );
+}
+
+export default Navbar;
