557a1b92f7d5b5d242682898928944ca7689423f
jnluis <jnluis@ua.pt>
2023-05-03 17:23:22 +0100

    Add react-router-dom to support multiple pages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import Footer from "./components/Footer";
-import Media from "./pages/media/media";
-import SettingsPage from "./pages/settings/settingsPage";
-import Clock from "./components/Date";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* 
-      Maybe only call App
-      Giving App a grid layout.
-      App calls rest of components
-    */}
-    <App className="isto talvez seja sempre necessário, porque senão a pagina fica scrollable" />
-    <Clock/>
-    <Media />
-    <Footer />
+    <App/>
+
   </React.StrictMode>
 );
