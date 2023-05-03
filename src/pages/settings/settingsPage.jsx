0868fc8675989d9d920b303d9ce027cc2669533a
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-03 12:43:19 +0100

    Add Some Settings Values

--- src/pages/settings/settingsPage.jsx
+++ src/pages/settings/settingsPage.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 
-const values = {};
+const values = {
+  Appearance: {
+    Theme: "Blue",
+    Brightness: "50%",
+    NightLight: "Off",
+  },
+  TextNFont: {
+    Font: "Arial",
+    FontSize: "14px",
+    ReadAloud: "Off",
+  },
+  Accessibility: {
+    ColourBlindMode: "Off",
+    UnderlineLinks: "On",
+  },
+  SoundNVideo: {
+    Volume: "50%",
+    Mute: "Off",
+    AutoPlay: "Off",
+    OutputDevice: "Speakers",
+  },
+};
 
 function SettingsPage() {
   return (
     <div className="flex flex-row">
-      <div className="basis-2/5">Navbar</div>
-      <div className="basis-3/5 flex-nowrap">Actual Settings</div>
+      <div className="basis-1/3">Navbar</div>
+      <div className="basis-2/3 flex-nowrap">Actual Settings</div>
     </div>
   );
 }
