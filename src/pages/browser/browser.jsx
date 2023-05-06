ae16bdfde2b503874b1547be2bfdc8c5b1ba16f5
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-05-06 01:54:20 +0100

    try at embedding browser

--- src/pages/browser/browser.jsx
+++ src/pages/browser/browser.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function Browser() {
   return (
-    <>escreve código aqui Aguiar!!</>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Helmet>
+        <script type="module" src="https://unpkg.com/x-frame-bypass"></script>
+      </Helmet>
+      <iframe is = "x-frame-bypass" src="https://www.google.com" style={{ width: '80%', height: '80%' }}></iframe>
+    </div>
   );
 }
 
