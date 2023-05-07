1c8d6e1eb34dffd6a1c7c37a5801851d50a9b69f
jnluis <jnluis@ua.pt>
2023-05-07 15:58:30 +0100

    Break navbar in components, make buttons appear only when they are necessary and also import MusicPlayer to Navbar

---
+++ src/components/FooterComponents/MediaButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MediaButton() {
+  return (
+    <button className="h-[100px] w-[100px] text-black font-semibold bg-white rounded-full text-[14px]">
+        <svg width="48" height="41" viewBox="0 0 48 41" className="ml-7 mb-2" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M48 20.5L-9.53674e-07 41L-9.53674e-07 0L48 20.5Z" fill="black" />
+        </svg>
+        Media
+        </button>
+  );
+}
+
+export default MediaButton;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import Slider from "./components/slider.jsx"
-import MusicPlayer from "./components/MusicPlayer.jsx"
 
 function Home() {
-  const [value, setValue] = useState(40);
-
-  const handleOnChange = (e) => {
-    setValue(e.target.value);
-  };
 
   return (
     <>
@@ -15,7 +9,6 @@ function Home() {
     <div className="fixed top-[370px] right-10 h-200 flex flex-col justify-center items-center">
     <Slider />
     </div>
-    <MusicPlayer />
 
     </>
   );
