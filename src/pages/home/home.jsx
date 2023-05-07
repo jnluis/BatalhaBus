b1c1839a6a7fde3b9b13912b1be32e9b9607063d
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-05-07 20:45:37 +0100

    recover dropped stash
    dumb af

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import Slider from "./components/slider.jsx"
+import TripDetails from "../../assets/TripDetails.svg";
+import YTIcon from "../../assets/YTicon.svg";
+import GoogleIcon from "../../assets/Browsericon.svg";
+import GamesIcon from "../../assets/Gamesicon.svg";
 import Weather from "./components/Weather.jsx"
 
 function Home() {
+
+  const buttonStyle = {
+    width: '142px',
+    height: '127px',
+    margin: '0 120px',
+    borderRadius: '10px',
+    backgroundColor: '#E6B78C',
+    color: 'black',
+    //display: 'inline-flex', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    textAlign: 'center',
+    fontSize : '22px',
+    color: 'black',
+    font : 'normal normal bold 22px/27px Poppins',
+  }
+
+  const buttonDivStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'relative',
+    top : '425px'
+  }
 
   return (
     <>
@@ -12,6 +39,25 @@ function Home() {
     <div className="fixed top-[370px] right-10 h-200 flex flex-col justify-center items-center">
     <Slider />
     </div>
+    <div style= {buttonDivStyle}>
+      <button style={buttonStyle}>
+        <img src = {TripDetails} alt = "Trip Details" style={{ position: 'relative', left: '30%' , fontsize : '22px', marginBottom : '10px' }}/>
+        Trip Details
+      </button>
+      <button style={buttonStyle}>
+        <img src = {YTIcon} alt = "Media" style={{ position: 'relative', left: '30%' , fontsize : '22px', marginBottom : '10px' }}/>
+        Media
+      </button>
+      <button style={buttonStyle}>
+        <img src = {GoogleIcon} alt = "Browser" style={{ position: 'relative', left: '28%' , fontsize : '22px' , marginBottom : '10px'}}/>
+        Browser
+      </button>
+      <button style={buttonStyle}>
+        <img src = {GamesIcon} alt = "Games" style={{ position: 'relative', left: '28%' , fontsize : '22px' , marginBottom : '10px'}}/>
+        Games
+      </button>
+    </div>
+    
 
     </>
   );
