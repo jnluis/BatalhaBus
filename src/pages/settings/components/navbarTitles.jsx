cb682645adcf2358072247ff6701539499b5a9c7
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-10 18:26:10 +0100

    Update navbarTitles.jsx

--- src/pages/settings/components/navbarTitles.jsx
+++ src/pages/settings/components/navbarTitles.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
-function NavbarTitles() {
+function NavbarTitles(props) {
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-lg text-center"></div>
+    <div className="text-black p-4 rounded-lg text-center border-t-2 border-black">
+      {props.title}
+    </div>
   );
 }
-
 export default NavbarTitles;
