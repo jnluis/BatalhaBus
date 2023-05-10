6f232d60770ff614fff4cc7a9106dd3f53eb29ec
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-10 18:02:38 +0100

    Update settingsPage.jsx

--- src/pages/settings/settingsPage.jsx
+++ src/pages/settings/settingsPage.jsx
@@ -1,31 +1,9 @@
-import React, { Component } from "react";
-
-const values = {
-  Appearance: {
-    Theme: "Blue",
-    Brightness: "50%",
-    NightLight: "Off",
-  },
-  TextNFont: {
-    Font: "Arial",
-    FontSize: "14px",
-    ReadAloud: "Off",
-  },
-  Accessibility: {
-    ColourBlindMode: "Off",
-    UnderlineLinks: "On",
-  },
-  SoundNVideo: {
-    Volume: "50%",
-    Mute: "Off",
-    AutoPlay: "Off",
-    OutputDevice: "Speakers",
-  },
-};
+import React from "react";
+import NavbarTitles from "./components/navbarTitles";
+import Settings from "./components/settings";
+import Navbar from "./components/navbar.jsx";
 
 function SettingsPage() {
-<<<<<<< Updated upstream
-=======
   const values = {
     Appearance: {
       Theme: "Blue",
@@ -73,19 +51,47 @@ function SettingsPage() {
     settingsCategories.push(
       <div
         key={index}
-        className="text-black p-4 rounded-lg border-t-2 border-black mb-16 "
+        className="text-black p-4 rounded-lg border-t-2 border-black"
       >
-        <span className="px-16 pb-16">{category}</span>
+        <span className="px-16">{category}</span>
         <span className="my-2">{settings}</span>
       </div>
     );
   });
 
->>>>>>> Stashed changes
   return (
-    <div className="flex flex-row">
-      <div className="basis-1/3">Navbar</div>
-      <div className="basis-2/3 flex-nowrap">Actual Settings</div>
+    <div className="w-full h-full flex flex-col">
+      <div className="flex flex-row max-h-128">
+        <div className="basis-1/3 border-r-4 border-black pl-8 pr-16">
+          <Navbar
+            titles={[
+              "Appearance",
+              "Text & Font",
+              "Accessibility",
+              "Sound & Video",
+            ]}
+          />
+        </div>
+        <div className="basis-2/3 flex-nowrap pl-16 pr-8 overflow-auto">
+          {settingsCategories}
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+          <div className="pl-16">q</div>
+        </div>
+      </div>
     </div>
   );
 }
