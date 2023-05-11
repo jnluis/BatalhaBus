b14c73bdd1795de7d4a258ea4a1dd0d0e31db839
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-11 12:19:08 +0100

    Fixed Buttons

--- src/pages/settings/components/Icons/Off.jsx
+++ src/pages/settings/components/Icons/Off.jsx
@@ -3,13 +3,67 @@ import React from "react";
 const Off = () => {
   return (
     <svg
-      width="670"
-      height="272"
-      viewBox="0 0 670 272"
+      width="79"
+      height="43"
+      viewBox="0 0 79 43"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="7.07483" y="1.79102" width="662" height="270" fill="#D9D9D9" />
+      <g filter="url(#filter0_d_177_3995)">
+        <g clip-path="url(#clip0_177_3995)">
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M22 34.993C21.8339 34.9977 21.6672 35 21.5 35C11.835 35 4 27.165 4 17.5C4 7.83502 11.835 0 21.5 0C21.6672 0 21.8339 0.00234552 22 0.00700546V0H57V0.00700546C57.1661 0.00234552 57.3328 0 57.5 0C67.165 0 75 7.83502 75 17.5C75 27.165 67.165 35 57.5 35C57.3328 35 57.1661 34.9977 57 34.993V35H22V34.993Z"
+            fill="#C20303"
+          />
+        </g>
+        <circle cx="21.5" cy="17.5" r="17.5" fill="#FEFEFE" />
+        <path
+          d="M25.7909 18L31.4767 12.3142C32.1744 11.6165 32.1744 10.4852 31.4767 9.78693L30.2131 8.5233C29.5153 7.82557 28.3841 7.82557 27.6858 8.5233L22 14.2091L16.3142 8.5233C15.6165 7.82557 14.4852 7.82557 13.7869 8.5233L12.5233 9.78693C11.8256 10.4847 11.8256 11.6159 12.5233 12.3142L18.2091 18L12.5233 23.6858C11.8256 24.3835 11.8256 25.5148 12.5233 26.2131L13.7869 27.4767C14.4847 28.1744 15.6165 28.1744 16.3142 27.4767L22 21.7909L27.6858 27.4767C28.3835 28.1744 29.5153 28.1744 30.2131 27.4767L31.4767 26.2131C32.1744 25.5153 32.1744 24.3841 31.4767 23.6858L25.7909 18Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_177_3995"
+          x="0"
+          y="0"
+          width="79"
+          height="43"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_177_3995"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_177_3995"
+            result="shape"
+          />
+        </filter>
+        <clipPath id="clip0_177_3995">
+          <rect width="71" height="35" fill="white" transform="translate(4)" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };
