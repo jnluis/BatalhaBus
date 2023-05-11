c425cc26981819c29f1e2ccda4e65166feac3373
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-11 01:57:32 +0100

    Add On/Off Button for future support

---
+++ src/pages/settings/components/Icons/Off.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Off = () => {
+  return (
+    <svg
+      width="670"
+      height="272"
+      viewBox="0 0 670 272"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="7.07483" y="1.79102" width="662" height="270" fill="#D9D9D9" />
+    </svg>
+  );
+};
+
+export default Off;
