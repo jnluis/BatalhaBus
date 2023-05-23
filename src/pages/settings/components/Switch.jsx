cd40b330d0e5fbff052e3252f561a3803c57b47a
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-05-23 16:08:57 +0100

    Properly toggle On and Off

--- src/pages/settings/components/Switch.jsx
+++ src/pages/settings/components/Switch.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import On from "./Icons/On";
 import Off from "./Icons/Off";
 
-const Switch = ({ isOn, handleToggle }) => {
-  return <div onClick={handleToggle}>{isOn ? <On /> : <Off />}</div>;
-};
+function Switch({ isOn, onToggle }) {
+  const handleClick = () => {
+    onToggle(!isOn);
+  };
+
+  return <div onClick={handleClick}>{isOn ? <On /> : <Off />}</div>;
+}
 
 export default Switch;
